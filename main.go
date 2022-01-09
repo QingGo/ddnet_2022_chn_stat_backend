ad650df55b6a4aaf9b66931aec96343f2e742ad4
QingGo <zyqingjohn@qq.com>
2022-01-10 01:09:45 +0800

    allow cors when debug

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 
@@ -16,8 +17,14 @@ func main() {
 	if csvFilePath == "" {
 		csvFilePath = "../results_example.csv"
 	}
+	isProduct := os.Getenv("DEBUG")
 	db.Init(csvFilePath)
 	router := gin.Default()
+	if isProduct == "" {
+		config := cors.DefaultConfig()
+		config.AllowOrigins = []string{"*"}
+		router.Use(cors.New(config))
+	}
 	router.GET("/user/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.JSON(http.StatusOK, db.Find(name))
