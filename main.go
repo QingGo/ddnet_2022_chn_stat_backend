58add27423c061aab1760e0c017107e37c06e182
QingGo <zyqingjohn@qq.com>
2022-01-10 20:46:18 +0800

    remove anti cors feature

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 
@@ -17,14 +16,8 @@ func main() {
 	if csvFilePath == "" {
 		csvFilePath = "../results_example.csv"
 	}
-	isProduct := os.Getenv("DEBUG")
 	db.Init(csvFilePath)
 	router := gin.Default()
-	if isProduct == "" {
-		config := cors.DefaultConfig()
-		config.AllowOrigins = []string{"*"}
-		router.Use(cors.New(config))
-	}
 	router.GET("/user/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.JSON(http.StatusOK, db.Find(name))
